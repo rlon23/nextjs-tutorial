d19b4367a8e97d254b970f070eab0403aa6a4e1f
Arlony Diaz <>
2024-10-17 18:21:56 -0500

    add loading component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import Link from 'next/link';
 
 function HomePage() {
   return (
     <div>
-      <h1 className='text-7xl'>HomePage</h1>;
+      <h1 className='text-7xl'>HomePage</h1>
+      <Link href='/about' className='text-xl text-blue-500 inline-block mt-8'>
+        about page
+      </Link>
     </div>
   );
 }
